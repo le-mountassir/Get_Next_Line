83d8149884847a4cec0add5181a401f96f90699e
ahmed-elmountassir <ahmedelmountassir10@gmail.com>
2021-11-16 12:59:21 +0100

    utils update

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,14 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahel-mou <ahel-mou@student-1337.ma>        +#+  +:+       +#+        */
+/*   By: ahel-mou <ahel-mou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 17:34:01 by ahel-mou          #+#    #+#             */
-/*   Updated: 2021/11/15 17:34:01 by ahel-mou         ###   ########.fr       */
+/*   Updated: 2021/11/16 11:35:29 by ahel-mou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+size_t  ft_strlen(char *cara)
+{
+    size_t  len;
+
+    len = 0;
+    while (cara[len])
+        len++;
+    return (len);   
+}
 
 int		line_len(char *cara)
 {
@@ -20,18 +30,25 @@ int		line_len(char *cara)
 	return (len);
 }
 
-size_t  ft_strlen(char *cara)
+char    *ft_strdup(char *s)
 {
-    size_t  len;
+        size_t          i;
+        char            *str;
 
-    len = 0;
-    while (cara[len])
-        len++;
-    return (len);
-        
+        i = 0;
+        str = (char *)malloc(sizeof(char) * ft_strlen(s) + 1);
+        if (!str)
+                return (NULL);
+        while (s[i])
+        {
+                str[i] = s[i];
+                i++;
+        }
+        str[i] = '\0';
+        return (str);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*str;
 	size_t	i;
@@ -59,18 +76,14 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	return (str);
 }
 
-char	*line_mallocater(char *string)
-{
-	int		len;
-	char	*rt;
-	int		i;
+// char	*line_mallocater(char *string)
+// {
+// 	int		len;
+// 	char	*rt;
+// 	int		i;
 
-	i = 0;
-	len = line_len(string);
-	rt = (char *)malloc(sizeof(char) * len + 1);
-	while (i < len)
-	{
-		rt[i] 
-	}
-
-}
+// 	i = 0;
+// 	len = line_len(string);
+// 	rt = (char *)malloc(sizeof(char) * len + 1);
+// 	while (i++ < len)
+// 		rt[i] = string[i];
