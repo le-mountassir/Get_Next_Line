831ce0d82dc4d8994d58deee13665ebf91db540f
ahmed-elmountassir <ahmedelmountassir10@gmail.com>
2021-11-16 12:59:06 +0100

    main function

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahel-mou <ahel-mou@student-1337.ma>        +#+  +:+       +#+        */
+/*   By: ahel-mou <ahel-mou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 17:34:08 by ahel-mou          #+#    #+#             */
-/*   Updated: 2021/11/15 17:34:08 by ahel-mou         ###   ########.fr       */
+/*   Updated: 2021/11/16 11:56:32 by ahel-mou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,26 @@ char    *get_next_line(int  fd)
 {
 	char			*tmp_arr;
 	static char		*stc_arr;
-	int				n_b_file;
-
+	// int				n_b_file;
+	int				checker;
+	size_t read_f;
+	checker = 1;
+	
+	printf("File_D = %d\n", fd);
+	
 	if (fd >= 0 && BUFFER_SIZE > 0)
 	{
-		tmp_arr = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
-		if (!tmp_arr)
-			return (NULL);
-		return
-
+		tmp_arr = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		read_f = read(fd, tmp_arr, BUFFER_SIZE);
+		tmp_arr[read_f] = '\0';
+		printf("READ VAL = %zu\nSTR_IMPORTED =\n%s", read_f, tmp_arr);
+		
+		printf("\n static arr  = %s", ft_strjoin(stc_arr, tmp_arr));
 	}
 	return (NULL);
+}
+int main ()
+{
+	int fd = open("get_next_line.h", O_RDONLY);
+	get_next_line(fd);
 }
