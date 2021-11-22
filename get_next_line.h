36a84c04b47358fcf79078f1ac974ebed12249e5
ahmed-elmountassir <ahmedelmountassir10@gmail.com>
2021-11-22 08:15:30 +0100

    header update

--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,15 @@
 /*   By: ahel-mou <ahel-mou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 13:56:20 by ahel-mou          #+#    #+#             */
-/*   Updated: 2021/11/18 13:56:20 by ahel-mou         ###   ########.fr       */
+/*   Updated: 2021/11/21 15:27:35 by ahel-mou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
-#endif
-
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
 
 # include <stdio.h>
 # include <string.h>
@@ -33,7 +31,7 @@ typedef struct dipo_dial_variablat
 	char	*tmp2;
 	char	*buff;
 	char	*cur_line;
-}			storage;
+}			t_storage;
 
 size_t		ft_strlen(const char *cara);
 void		*ft_memcpy(void *dest, const void *src, size_t n);
